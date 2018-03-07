9b233c440a30af6ba0f5c595436691ddf9caeb54
chenyinkai <287696322@qq.com>
2018-03-07 12:10:18 +0800

    update

--- server/lib/default.js
+++ server/lib/default.js
@@ -3,8 +3,8 @@ const config = {
   port: 3000,
   database: {
     DATABASE: 'koamysql', // 数据库名称
-    USERNAME: 'root', // 用户
-    PASSWORD: 'root', // 密码
+    USERNAME: 'your name', // 用户
+    PASSWORD: 'your password', // 密码
     PORT: '3306', // 端口
     HOST: 'localhost' // 服务ip地址
   }
